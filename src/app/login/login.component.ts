e728ef1db93f4ad58895c225717736c96a3bfb44
Gonza-5150 <estevesgonzalo@hotmail.com>
2023-06-23 20:38:34 -0300

    login component ext

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  username ="Tu vieja"
+  password = ''
 
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  handleLogin() {
+    console.log(this.username);
+    //console.log(this.password);
+  }
 }
