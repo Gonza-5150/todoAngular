174a3be2523c0ae47dc588ee41eeef303563ba84
Gonza-5150 <estevesgonzalo@hotmail.com>
2023-06-25 19:26:40 -0300

    route guard and logout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
   invalidLogin = false
 
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private hardcodedAuthenticationService: HardcodedAuthenticationService) {}
 
   ngOnInit(): void {
       
@@ -21,7 +23,8 @@ export class LoginComponent implements OnInit {
 
   handleLogin() {
     console.log(this.username);
-    if(this.username==="Gonza" && this.password==='tanga') {
+    //if(this.username==="Gonza" && this.password==='tanga') {
+      if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
       //redirect to Welcome
       this.router.navigate(['/welcome', this.username])
       this.invalidLogin = false
